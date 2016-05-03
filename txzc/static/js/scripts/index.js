9848c4973b7b7e2eb05ae97a6728283bea2bd69a
missMHWGO <2486148831@qq.com>
2016-05-03 13:54:19 +0800

    删除随机生成的话，调整样式，分享后点击屏幕跳回前一页

--- txzc/static/js/scripts/index.js
+++ txzc/static/js/scripts/index.js
@@ -7,7 +7,7 @@
     canvas.width = win.innerWidth;
     canvas.height = win.innerHeight;
 
-    var say;
+    // var say;
 
     var image_nopic = new Image();
     image_nopic.src = 'static/img/nopic.png';
@@ -31,10 +31,11 @@
         ctx.stroke();
 
         ctx.fillStyle = '#fff';
-        ctx.font = '0.24' * rem + 'px' + ' 宋体';
+        ctx.font = '0.33' * rem + 'px' + ' 宋体';
         ctx.textBaseline = 'bottom';
 
-        ctx.fillText('2016年', 7.6 * rem, 0.48 * em);
+        ctx.fillText('2016年', 7.49 * rem, 0.53 * em);
+        alert(2);
 
         var image_logo = new Image();
         image_logo.src = 'static/img/logo.png';
@@ -47,16 +48,16 @@
             ctx.drawImage(this, 0.4 * rem, 1.6 * rem, 15.4 * rem, 3 * rem);
         }
 
-        var saying = new Array("哈哈", "呵呵", "呀呀", "嘿嘿", "啦啦", "奔跑吧，创业者！", "哦哦", "噢噢");
-        var index = Math.floor(saying.length * Math.random());
-        say = saying[index];
-        $.get('static/font/access.php?lang="SSZH"').done(function() {
-            setTimeout(function() {
-                ctx.font = '0.6' * rem + 'px' + ' SSZH';
-                ctx.fillText(say, 0.5 * rem, 13.6 * em);
-            }, 500);
+        // var saying = new Array("哈哈", "呵呵", "呀呀", "嘿嘿", "啦啦", "奔跑吧，创业者！", "哦哦", "噢噢");
+        // var index = Math.floor(saying.length * Math.random());
+        // say = saying[index];
+        // $.get('static/font/access.php?lang="SSZH"').done(function() {
+        //     setTimeout(function() {
+        //         ctx.font = '0.6' * rem + 'px' + ' SSZH';
+        //         ctx.fillText(say, 0.5 * rem, 13.6 * em);
+        //     }, 500);
 
-        });
+        // });
 
         var image_code = new Image();
         image_code.src = 'static/img/code.png';
@@ -80,8 +81,8 @@
             self.bind();
         },
         view: function() {
-            canvas.style.width = win.innerWidth - 1.2 * rem + 'px';
-            canvas.style.height = win.innerHeight - 3.6 * rem + 'px';
+            canvas.style.width = win.innerWidth - 1.2 * rem + 'px'; //14.8rem
+            canvas.style.height = win.innerHeight - 3.6 * rem + 'px'; //16em-3.6rem
             // document.getElementsByClassName('textbg')[0].style.height = win.innerHeight - 3.6 * rem + 'px';
             // document.getElementsByClassName('textbg')[0].style.width = win.innerWidth - 1.2 * rem + 'px';
             // document.getElementsByClassName('textbg')[1].style.height = win.innerHeight - 3.6 * rem + 'px';
@@ -126,44 +127,44 @@
                     setTimeout(function() {
                         ctx.font = '1.35' * rem + 'px' + ' YGYDF';
                         if (zc.value == "") {
-                        	zc.value = "众创年度人物专访";
+                            zc.value = "请输入主题内容";
                         }
-                        ctx.fillText(zc.value.split("").join(" "), 0.41 * rem, 12 * em);                        	
-                        
+                        ctx.fillText(zc.value.split("").join(" "), 0.41 * rem, 12 * em);
+
                         ctx.fillStyle = '#f4ba15'; //f4ba15
                         var profile = $(".profile")[0];
                         $.get('static/font/access.php?lang="SSZH"').done(function() {
                             setTimeout(function() {
                                 ctx.font = '0.95' * rem + 'px' + ' SSZH';
                                 if (profile.value == "") {
-                                	profile.value = "腾讯众创空间 侯晓楠";
+                                    profile.value = "请输入公司名和姓名";
                                 }
-                                ctx.fillText(profile.value, 0.45 * rem, 12.9 * em);                               	                                
+                                ctx.fillText(profile.value, 0.45 * rem, 12.9 * em);
 
                                 //var image = canvas.toDataURL("image/png").replace('image/png', 'image/octet-stream');
                                 //window.location.href = image; 
                                 var image = canvas.toDataURL();
 
 
-                                $.post("./img.php",{
-                                	'image': image
-                                }).done(function(data){
-                                	//alert(data);
-                                	// $("#download")[0].href = data;
-                                	// $("#download img").click();
-                                	alert('长按图片保存到本地！');
-                                	$("#bg")[0].style.zIndex = "-1";
-                                	canvas.style.display = "none";
-                    				$("#text")[0].style.display = "none";                   				
-                    				$("#download").css({
-                    					'width': canvas.style.width,
-                    					'height': canvas.style.height,
-                    					'margin-left': '0.6'*rem+'px',
-                    					'margin-top': '0.65'*rem+'px',
-                    					'display': 'block'
-                    				});
-                    				$("#download")[0].src = data; 
-                    				                  				                   				
+                                $.post("./img.php", {
+                                    'image': image
+                                }).done(function(data) {
+                                    //alert(data);
+                                    // $("#download")[0].href = data;
+                                    // $("#download img").click();
+                                    alert('长按图片保存到本地！');
+                                    $("#bg")[0].style.zIndex = "-1";
+                                    canvas.style.display = "none";
+                                    $("#text")[0].style.display = "none";
+                                    $("#download").css({
+                                        'width': canvas.style.width,
+                                        'height': canvas.style.height,
+                                        'margin-left': '0.6' * rem + 'px',
+                                        'margin-top': '0.65' * rem + 'px',
+                                        'display': 'block'
+                                    });
+                                    $("#download")[0].src = data;
+
                                 });
 
                                 // var saveFile = function(data, filename) {
@@ -195,8 +196,18 @@
                 $('#sharetext')[0].style.display = 'block';
             }
 
+            $('#sharebg')[0].onclick = function() {
+                $('#sharebg')[0].style.display = 'none';
+                $('#sharetext')[0].style.display = 'none';
+            }
+
+            var scale = (14.8 * rem) / (16 * em - 3.6 * rem);
+            // alert(scale);
+            var clip_width = 10 * rem;
+            var clip_height = clip_width / scale;
+
             $("#clipArea").photoClip({
-                size: [10 * rem, 10 * em],
+                size: [clip_width, clip_height],
                 outputSize: [640, 640],
                 file: "#file",
                 view: "#view",
@@ -255,10 +266,10 @@
                         ctx.stroke();
 
                         ctx.fillStyle = '#fff';
-                        ctx.font = '0.24' * rem + 'px' + ' 宋体';
+                        ctx.font = '0.33' * rem + 'px' + ' 宋体';
                         ctx.textBaseline = 'bottom';
 
-                        ctx.fillText('2016年', 7.6 * rem, 0.48 * em);
+                        ctx.fillText('2016年', 7.49 * rem, 0.53 * em);
 
                         var image_logo = new Image();
                         image_logo.src = 'static/img/logo.png';
@@ -271,8 +282,8 @@
                             ctx.drawImage(this, 0.4 * rem, 1.6 * rem, 15.4 * rem, 3 * rem);
                         }
 
-                        ctx.font = '0.6' * rem + 'px' + ' SSZH';
-                        ctx.fillText(say, 0.5 * rem, 13.6 * em);
+                        // ctx.font = '0.6' * rem + 'px' + ' SSZH';
+                        // ctx.fillText(say, 0.5 * rem, 13.6 * em);
 
                         var image_code = new Image();
                         image_code.src = 'static/img/code.png';
